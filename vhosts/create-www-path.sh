8129d4afbe658ddada7c49136e734d650acae8c1
marlon.ruescher <m.ruescher@gds-gmbh.de>
2016-12-05 18:17:32 +0100

    fixed bug at modifing users home directory

--- vhosts/create-www-path.sh
+++ vhosts/create-www-path.sh
@@ -16,7 +16,7 @@ read sftp_user
 usermod -g sftp $sftp_user
 
 # set new home directory for sftp user
-usermod -d /var/www/${www_domain}
+usermod -d /var/www/${www_domain} $sftp_user
 
 # set right directory owner
 chown root:sftp /var/www/${www_domain}
